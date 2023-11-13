91239cfb3ce2ef6fa0d885aaeef224d47be205de
DuaaAzhar <74790476+DuaaAzhar@users.noreply.github.com>
2023-11-13 06:43:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as new version
+
+//alert (3+3); //will give error bcz we are using nodejs not browser
+
+console.log(3 
+    +
+     3) // wrong method to write bcz code readability should be high
+
+// primitive data types
+
+     let name = "hitesh"
+     let age = 18
+     let isLoggedIn = false
+     let state;
+
+// number 2 >> 53
+// bigint
+//string => ""
+//boolean => true/false
+//null =? standalone value
+// undefined
+//symbol => unique
+
+
+
+// non - primitive data types
+
+// object
+
+
+
+console.log (typeof undefined); // undefined
+console.log(typeof null); //object
