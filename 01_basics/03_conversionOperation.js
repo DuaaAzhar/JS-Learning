59e6ce96073d698335bf0896864721d25856a13b
DuaaAzhar <duaaazhar04@gmail.com>
2024-09-25 13:01:36 +0500

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = null
+
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+
+
+// "33" => 33
+// "3abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1, false =>0
+
+
+let isLoggedIn= 100
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "duaa" => true
+// 100 => true
+// undefined/ NaN => false, "NaN" => true
+
+
+let num = 33
+let stringNum = String(num)
+console.log(typeof(stringNum))
