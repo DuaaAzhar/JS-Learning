ebe2540df1ad9433572194764ee46ad018bc0f76
DuaaAzhar <duaaazhar04@gmail.com>
2024-09-25 16:28:03 +0500

    operators study

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,31 +1,80 @@
-let score = null
+// let score = null
 
 
-let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
 
 
 
-// "33" => 33
-// "3abc" => NaN
-// null => 0
-// undefined => NaN
-// true => 1, false =>0
+// // "33" => 33
+// // "3abc" => NaN
+// // null => 0
+// // undefined => NaN
+// // true => 1, false =>0
 
 
-let isLoggedIn= 100
-let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn= 100
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
-// 1 => true; 0 => false
-// "" => false
-// "duaa" => true
-// 100 => true
-// undefined/ NaN => false, "NaN" => true
+// // 1 => true; 0 => false
+// // "" => false
+// // "duaa" => true
+// // 100 => true
+// // undefined/ NaN => false, "NaN" => true
 
 
-let num = 33
-let stringNum = String(num)
-console.log(typeof(stringNum))
+// let num = 33
+// let stringNum = String(num)
+// console.log(typeof(stringNum))
+
+
+
+//  *****************OPERATORS**********************
+console.log("1" + 2);   // 12
+console.log(1 + "2");   // 12
+console.log("1" + 2 + 2);  //122
+console.log(1 + 2 + "2");  //32
+
+
+
+console.log(1 + 2 * 3 / 4); //2.5 follows DMAS rule
+console.log(1 + 2 * 8 % 4); //1 follows DMAS rule
+
+
+console.log(+true) //1
+console.log(+"") //0
+console.log(-true) //-1
+console.log(-"") //-0
+
+
+
+
+let num1 = num2 = num3= 2+2
+console.log(num1) //4
+
+
+
+let counter = 100
+++counter
+console.log(counter)
+
+
+let a= 3;
+
+let c = a++;  // c= 3 will set the value of a before increment
+console.log(a) //a=4 as value of a will be changed by this operator
+
+console.log(c)
+let b = ++a; 
+console.log(b) //b=5 it will set the value after incrementing 
+console.log(a) //a=5 
+
+
+
+
+
+
+
