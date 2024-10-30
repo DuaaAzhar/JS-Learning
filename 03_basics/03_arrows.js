3286f2e138a06b1007294c5a9d5cdf26de7f30eb
DuaaAzharBIM <duaa.azhar@bigimmersive.com>
2024-10-30 15:24:37 +0500

    arrowFunctions

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -24,6 +24,8 @@ function logging() {
 function thisInFunction() {
     let username = "Duaa"
     console.log(this.username); //will be undefined
+    console.log("this in regular function");
+    console.log(this);
 }
 
 thisInFunction()
@@ -31,10 +33,13 @@ thisInFunction()
 const thisInArrowFunc= () => {
     let username = "Duaa"
     console.log(this.username);
+    console.log("this in arrow function");
+    console.log(this);
+    
 }
 
 thisInFunction()
-
+thisInArrowFunc()
 
 // ARROW FUNCTIONS
 
@@ -49,4 +54,54 @@ console.log(method2(1,2))
 
 const returnObj = () => ({username: "Duaa", price: 2000})
 console.log(returnObj())
+
+
+
+
+
+//Handling of "this" in REGULAR Functions VS ARROW Functions
+// In regular functions the this keyword represented the object that called the function, which could be the window, the document, a button or whatever.
+// With arrow functions the this keyword always represents the object that defined the arrow function.
+
+
+// for example in regular functions
+
+// .....................Regular Function:
+
+// hello = function() {
+//     document.getElementById("demo").innerHTML += this;
+//   }
+  
+//   // The window object calls the function:
+//   window.addEventListener("load", hello);
+  
+//   // A button object calls the function:
+//   document.getElementById("btn").addEventListener("click", hello);
+
+
+// it will output 
+// [object Window]
+
+// after clicking button
+// [object Window][object HTMLButtonElement]
+
+
+// ...................ARROW Function
+
+// Arrow Function:
+// hello = () => {
+//     document.getElementById("demo").innerHTML += this;
+//   }
+  
+//   // The window object calls the function:
+//   window.addEventListener("load", hello);
+  
+//   // A button object calls the function:
+//   document.getElementById("btn").addEventListener("click", hello);
+
+
+// //  it will output
+// [object Window]
+// // after clicking button 
+// [object Window][object Window]
 
